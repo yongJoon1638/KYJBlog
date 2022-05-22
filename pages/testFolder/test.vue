0c66cf71d8f6fe19c3cc0c4a9d978a4b789f6cea
YongJoon <kyj010708@naver.com>
2022-05-22 13:31:23 +0900

    초기 커밋

---
+++ pages/testFolder/test.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img
+        src="/v.png"
+        alt="Vuetify.js"
+        class="mb-5"
+      >
+      <blockquote class="blockquote">
+        TestTest
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'testVue'
+}
+</script>
