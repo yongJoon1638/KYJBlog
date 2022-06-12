8a5829299c5ea41ebb6ff1cd49a1a42b4f74597f
YongJoon <kyj010708@naver.com>
2022-06-12 16:21:24 +0900

    네비 바 기능 적용 완료

--- components/HeadNav.vue
+++ components/HeadNav.vue
@@ -1,11 +1,15 @@
 <template>
   <v-app-bar app color="white" flat>
-    <v-tool-bar-title class="text-no-wrap" style="margin-right: 1rem; font-weight: bold;">Dev Blog </v-tool-bar-title>
-    <v-tabs color="grey darken-1" v-model="tab">
-        <v-tab v-for="link in links" :key="link">
-          {{ link }}
-        </v-tab>
-      </v-tabs>
+    <v-tool-bar-title
+      class="text-no-wrap"
+      style="margin-right: 1rem; font-weight: bold"
+      >Yong Joon`s Dev Blog
+    </v-tool-bar-title>
+    <v-tabs color="grey darken-1">
+      <v-tab v-for="link in links" :key="link.title" :to="link.link">
+        {{ link.title }}
+      </v-tab>
+    </v-tabs>
     <v-spacer></v-spacer>
     <v-responsive max-width="260">
       <v-text-field
@@ -23,8 +27,24 @@
 export default {
   data() {
     return {
-      links: ["Home", "About", "Category", "Updates"],
-      tab: "",
+      links: [
+        {
+          title: "Home",
+          link: "/",
+        },
+        {
+          title: "About",
+          link: "/about",
+        },
+        {
+          title: "Category",
+          link: "/category",
+        },
+        {
+          title: "Updates",
+          link: "/updates",
+        },
+      ],
     };
   },
 };
